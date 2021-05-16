78584540b1b667852a3e421e54bee9f0e440c52e
Cristian Suárez <cristiansuarezg7@gmail.com>
2021-05-16 21:59:02 +0200

    Models updated and cors added

---
+++ config/middleware.js
@@ -0,0 +1,10 @@
+module.exports = {
+  //...
+  settings: {
+    cors: {
+      enabled: true,
+      // headers: "*",
+      origin: ["http://localhost:1337", "http://localhost:3000"],
+    },
+  },
+};
