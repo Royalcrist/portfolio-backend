ba88c92d000f9098941a60cd53288e5b1298b3cb
Cristian Suárez <cristiansuarezg7@gmail.com>
2022-04-23 15:39:33 +0200

    heroku config

--- config/server.js
+++ config/server.js
@@ -1,9 +1,11 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  proxy: true,
+  url: env("MY_HEROKU_URL"),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '73ac2a4d26d3a89c46858d7af7aea698'),
+      secret: env("ADMIN_JWT_SECRET", "73ac2a4d26d3a89c46858d7af7aea698"),
     },
   },
 });
