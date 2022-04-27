943c1bd4fe618f5dd9926e972370c27012364cda
Cristian Suárez <cristiansuarezg7@gmail.com>
2022-04-27 21:28:34 +0200

    Schema modified for count projects

---
+++ api/projects/config/schema.graphql.js
@@ -0,0 +1,18 @@
+module.exports = {
+  query: `
+    projectsCount(locale: String, where: JSON): Int!
+  `,
+  resolver: {
+    Query: {
+      projectsCount: {
+        description: "Return the count of projects",
+        resolverOf: "application::projects.projects.count",
+        resolver: async (obj, options, ctx) => {
+          return await strapi.api.projects.services.projects.count(
+            options.where || {}
+          );
+        },
+      },
+    },
+  },
+};
